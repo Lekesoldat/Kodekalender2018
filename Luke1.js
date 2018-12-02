b4ed30cc4285927c02f71a309fe0484e56ab125f
Magnus L. Holtet <magnulho@gmail.com>
2018-12-02 13:39:23 +0100

    Initial commit.

---
+++ Luke1.js
@@ -0,0 +1,19 @@
+const fetch = require('node-fetch');
+const url = 'https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-vekksort.txt';
+
+fetch(url)
+  .then(response => response.text())
+  .then(text => {
+    const lines = text.split('\n').map(value => parseInt(value));
+
+    let total = 0;
+    let biggest = 0;
+
+    for (let number of lines) {
+      if (number >= biggest){
+        biggest = number;
+        total += biggest;
+      }
+    }
+    console.log(total); // 12920419
+  })
