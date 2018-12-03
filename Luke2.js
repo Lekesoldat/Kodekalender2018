2e7f8a1a891c9b81080416fb753e12f8176973b3
Magnus L. Holtet <magnulho@gmail.com>
2018-12-03 13:12:14 +0100

    Remove bad comments.

--- Luke2.js
+++ Luke2.js
@@ -1,19 +1,15 @@
 const fetch = require('node-fetch');
 
 const main = async () => {
-  // Grab text
   const response = await fetch('https://s3-eu-west-1.amazonaws.com/knowit-julekalender-2018/input-rain.txt');
   const lines = (await response.text()).split('\n');
 
   const slopes = {};
 
-  // Traverse all lines
   for (let line of lines) {
-    // RegEx for the pattern
     const match = line.match(/\((?<x1>\d+),(?<y1>\d+)\);\((?<x2>\d+),(?<y2>\d+)\)/);
     
     if (match) {
-      // Destructuring
       const {x1, y1, x2, y2} = match.groups;
       const slope = (y2 - y1) / (x2 - x1);
       
@@ -24,7 +20,6 @@ const main = async () => {
       }
     }
   }
-  // Return highest value
   console.log(Object.values(slopes).reduce((previous, current) => Math.max(previous, current), 0)); // => 324
 }
 
